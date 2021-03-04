3470df26bbb8324c9114af5166a0893303b8882f
MirjamWeirer <mirjam.weirer@gmx.at>
2021-03-04 11:08:13 +0100

    Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+//Kommentar
+/*
+  1. Klassenname muss Dateiname entsprechen
+  2. Ändern => Refector
+*/
+public class HelloWorld {
+    // 3. psvm+Enter generiert die main-Methode
+    public static void main(String[] args) {
+        //Startpunkt, fürs Programm
+        System.out.println("Hello World on day 4321");
+        //Liebes "System"
+        //wir wollen etwas "out" ausgeben
+        //print
+        //ln (neue Zeile)
+        //übergeben mit "..." einen Text
+        //Abkürzung "sout"+Enter =System.out.println();
+    }
+}
